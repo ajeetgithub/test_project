610a374333052e06278970356c5bec751bf67785
unknown <ajeet.bcs@gmail.com>
2019-11-12 22:59:00 +0530

    second commit

---
+++ db_test/dbo/Tables/Person.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [BusinessEntityID] INT          NOT NULL,
+    [PersonType]       NCHAR (2)    NOT NULL,
+    [Title]            NVARCHAR (8) NULL,
+    [FirstName]        VARCHAR (10) NOT NULL,
+    [MiddleName]       VARCHAR (10) NULL,
+    [LastName]         VARCHAR (10) NOT NULL,
+    PRIMARY KEY CLUSTERED ([BusinessEntityID] ASC)
+);
+
